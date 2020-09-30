392796806247c7e533bd0edb8df07b85625743d5
dn6049949 <9.10-tsumu@ezweb.ne.jp>
2020-09-30 21:56:51 +0900

    Modify IntersectionOfCircle

--- geometry/IntersectionOfCircle.py
+++ geometry/IntersectionOfCircle.py
@@ -1,11 +1,13 @@
-# 円R1(x1,y1,r1), 円R2(x2,y2,r2)の交点を求める(O(1))
-"https://shogo82148.github.io/homepage/memo/geometry/circle-cross.html"
+"""
+Calculate the intersections of 2 circles, R1(x1,y1,r1) and R2(x2,y2,r2) in O(1) times
+ref : https://shogo82148.github.io/homepage/memo/geometry/circle-cross.html"
+"""
 
-# 2円の中心間の距離の2乗
+
 def D2(x1,y1,x2,y2):
     return (x1-x2)**2+(y1-y2)**2
 
-# 2円が交点を持つか
+
 def check(R1,R2):
     x1,y1,r1 = R1
     x2,y2,r2 = R2
@@ -15,8 +17,8 @@ def check(R1,R2):
     else:
         return 0
 
-# 2円の交点を出力
-def point(R1,R2):
+
+def intersection(R1,R2):
     if not check(R1,R2):
         return []
     else:
