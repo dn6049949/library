0bc634492322502a5b74814d96d03316d0ed132e
dn6049949 <9.10-tsumu@ezweb.ne.jp>
2020-09-30 23:10:55 +0900

    Modify LowLink

--- graph/LowLink.py
+++ graph/LowLink.py
@@ -1,13 +1,43 @@
-# LowLink
 """
-橋と関節点の列挙
-O(|V|+|E|)
+LowLink (bridge and articulation)
+init : O(|V|+|E|)
 """
+
+import sys
+sys.setrecursionlimit(1000000)
 
 class LowLink:
     def __init__(self, v):
+        def dfs(x, par):
+            self.pre[x] = self.low[x] = self.cnt
+            self.cnt += 1
+            is_articulation = False
+            n = 0
+            for y in v[x]:
+                if self.pre[y] is None:
+                    n += 1
+                    lowy = dfs(y, x)
+                    if lowy < self.low[x]:
+                        self.low[x] = lowy
+                    if self.pre[x] <= lowy:
+                        if self.pre[x]:
+                            is_articulation = True
+                        if self.pre[x] < lowy:
+                            self.bridge.append((x,y))
+                else:
+                    if par != y and self.pre[y] < self.low[x]:
+                        self.low[x] = self.pre[y]
+
+            if par is None and n > 1:
+                is_articulation = True
+
+            if is_articulation:
+                self.articulation.append(x)
+
+            return self.low[x]
+
+
         self.size = len(v)
-        self.v = v
         self.pre = [None]*self.size
         self.low = [None]*self.size
         self.articulation = []
@@ -15,44 +45,4 @@ class LowLink:
         for x in range(self.size):
             if self.pre[x] is None:
                 self.cnt = 0
-                self.dfs(x, None)
-
-    def dfs(self, x, par):
-        self.pre[x] = self.low[x] = self.cnt
-        self.cnt += 1
-        is_articulation = False
-        n = 0
-        for y in self.v[x]:
-            if self.pre[y] is None:
-                n += 1
-                lowy = self.dfs(y, x)
-                if lowy < self.low[x]:
-                    self.low[x] = lowy
-                if self.pre[x] <= lowy:
-                    if self.pre[x]:
-                        is_articulation = True
-                    if self.pre[x] < lowy:
-                        self.bridge.append((x,y))
-            else:
-                if par != y and self.pre[y] < self.low[x]:
-                    self.low[x] = self.pre[y]
-
-        if par is None and n > 1:
-            is_articulation = True
-
-        if is_articulation:
-            self.articulation.append(x)
-
-        return self.low[x]
-
-
-n,m = map(int, input().split())
-v = [[] for i in range(n)]
-for i in range(m):
-    a,b = map(int, input().split())
-    a -= 1
-    b -= 1
-    v[a].append(b)
-    v[b].append(a)
-ans = LowLink(v)
-print(len(ans.bridge))
+                dfs(x, None)
