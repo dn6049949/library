bb63733ad0c01b830b7a8465a9dab2929c5ff858
dn6049949 <9.10-tsumu@ezweb.ne.jp>
2020-10-01 10:15:46 +0900

    Modify CRT comment

--- math/ChineseRemainderTheorem.py
+++ math/ChineseRemainderTheorem.py
@@ -1,12 +1,13 @@
-# 中国剰余定理 O(nlogmaxm)
 """
+Chinese Remainder Theorem : O(NlogmaxM)
+calculate an interger x (x ≡ r (mod m)), satisfies following equations
 x ≡ b1 (mod m1)
 x ≡ b2 (mod m2)
 …
 x ≡ bn (mod mn)
-の解x ≡ r (mod m)をO(nlogmaxm)で求める
-返り値:(r,m)
-解がない場合は(0,-1)を返す
+
+return : (r,m)
+m = -1 iff having no answer
 """
 
 def extgcd(a, b):
@@ -17,6 +18,7 @@ def extgcd(a, b):
         p, q, pre_p, pre_q = pre_p, pre_q, p-m*pre_p, q-m*pre_q
     return a, p, q
 
+
 def crt(equation):
     r, m = equation[0]
     for b, m2 in equation[1:]:
@@ -26,30 +28,3 @@ def crt(equation):
         m, r = m*(m2//g), r+m*((b-r)//g*p%(m2//g))
         r %= m
     return r, m
-
-def solve():
-    def f(n):
-        yield 1
-        i = 2
-        while i**2 <= n:
-            if n%i == 0:
-                m = n//i
-                yield i
-                if m != i:
-                    yield m
-            i += 1
-
-    n = int(inpt())
-    N = 2*n
-    k = float("inf")
-    for x in f(N):
-        eq = [[0,x],[-1,N//x]]
-        r,m = crt(eq)
-        if m >= 0 and r < k:
-            k = r
-    print(k)
-    return
-
-#Solve
-if __name__ == "__main__":
-    solve()
