98ae422e367553f12da9db85c10f430848884df1
dn6049949 <9.10-tsumu@ezweb.ne.jp>
2020-10-11 17:31:28 +0900

    Modify SWAG comment

--- structure/SlidingWindowAggregation.py
+++ structure/SlidingWindowAggregation.py
@@ -1,27 +1,16 @@
-# SlidingWindowAggretgation
-"""
-任意のモノイド(f(a,b))を載せられる
-1. append
-2. popleft
-3. f(a_i, a_(i+1), … a_j) (a_i, a_(i+1), … a_j in stack)
-の3つがすべてO(1)で計算、処理可能、空間はO(N)
-"""
-
 from collections import deque
 class SlidingWindowAggretgation:
-    def __init__(self, f = min, default = float("inf")):
-        self.default = default
+    """
+    f : モノイド
+    swag.append(x) : xを追加 O(1)
+    swag.popleft() : 左端を削除 O(1)
+    swag.get() : f(swag) を取得 O(1)
+    """
+    def __init__(self, f = min, e = float("inf")):
+        self.e = e
         self.f = f
         self.front_stack = deque()
         self.back_stack = deque()
-
-    def get(self):
-        res = self.default
-        if self.front_stack:
-            res = self.f(res, self.front_stack[-1][1])
-        if self.back_stack:
-            res = self.f(res, self.back_stack[-1][1])
-        return res
 
     def append(self, x):
         fx = x
@@ -38,3 +27,11 @@ class SlidingWindowAggretgation:
                 fx = self.f(x, self.front_stack[-1][1])
                 self.front_stack.append((x, fx))
         self.front_stack.pop()
+
+    def get(self):
+        res = self.e
+        if self.front_stack:
+            res = self.f(res, self.front_stack[-1][1])
+        if self.back_stack:
+            res = self.f(res, self.back_stack[-1][1])
+        return res
